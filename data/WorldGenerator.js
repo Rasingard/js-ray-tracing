bd407d5d2f2d270a8a323c2dbc6cbdfa6c1a057a
Alexandre Pereira Moraes <apereimo@everis.com>
2019-04-02 14:09:05 -0300

    Added sea on WorldGenerator

--- data/WorldGenerator.js
+++ data/WorldGenerator.js
@@ -4,7 +4,17 @@ class WorldGenerator {
 
     build(x, y, z, ready) {
         const _3DSPACE = new Map(x, y, z);
-        _3DSPACE.addMaterial(new Material(new Color(0, 0, 0)));
+        _3DSPACE.addMaterial(
+            new Material(
+                new Color(0, 0, 0)
+            )
+        );
+
+        _3DSPACE.addMaterial(
+            new Material(
+                new Color(112, 112, 112)
+            )
+        );
 
         this.loadImage(TEXTURES_BASE64, (tilesData) => { // Tiles 16x16
             _3DSPACE.addMaterial(
@@ -23,16 +33,35 @@ class WorldGenerator {
                 )
             );
 
+            _3DSPACE.addMaterial(
+                new Material(
+                    new Color(30, 30, 160),
+                    this.getTyleTexture(tilesData, 12, 14, 16),
+                )
+            );
+
+            _3DSPACE.addTexture(this.getTyleData(tilesData, 12, 14, 16));
+
             this.loadImage(HEIGHT_MAP_BASE64, (imageData) => {
+                // Set ground
                 for (let i = 0; i < x; i++) {
                     for (let k = 0; k < z; k++) {                        
                         const height = imageData.data[((k*imageData.width + i) * 4)] / 4;
                         for(let j = 0; j < height; j++) {
                             if(j > height - 2) {
-                                _3DSPACE.setAt(i,j,k, 1);
+                                _3DSPACE.setAt(i,j,k, 2); // default material 2
                             } else {
-                                _3DSPACE.setAt(i,j,k, 2);
+                                _3DSPACE.setAt(i,j,k, 3); // default material 3
                             }
+                        }
+                    }
+                }
+
+                // Set Sea
+                for (let i = 0; i < x; i++) {
+                    for (let k = 0; k < z; k++) {
+                        for(let j = 0; j < 3; j++) {
+                            if(!_3DSPACE.getAt(i,j,k)) _3DSPACE.setAt(i,j,k, 4); // default material 4
                         }
                     }
                 }
@@ -40,7 +69,7 @@ class WorldGenerator {
                 if(ready) ready();
             });
         });
-
+        
         return _3DSPACE;
     }
 
@@ -55,6 +84,23 @@ class WorldGenerator {
             callback(context.getImageData(0, 0, canvas.width, canvas.height));
         }
         image.src = image64;
+    }
+
+    getTyleData(imageData, ix, iy, tileSize) {
+        const tileData = new ImageData(tileSize, tileSize);
+        let oi, ci;
+        for(let x = 0; x < tileSize; x++) {
+            for(let y = 0; y < tileSize; y++) {
+                oi = Math.floor(((y + ix * tileSize) * imageData.width + (x + iy * tileSize)) * 4);
+                ci = Math.floor((y * tileSize + x) * 4);
+                tileData.data[ci] = imageData.data[oi];
+                tileData.data[ci + 1] = imageData.data[oi + 1];
+                tileData.data[ci + 2] = imageData.data[oi + 2];
+                tileData.data[ci + 3] = imageData.data[oi + 3];
+            }
+        }
+
+        return tileData;
     }
 
     getTyleTexture(imageData, ix, iy, tileSize) {
